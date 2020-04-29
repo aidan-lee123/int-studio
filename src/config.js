140dbd7240d4ef283b17135188a57f3799489dbe
aidan-lee123 <aidan.lee123@gmail.com>
2020-04-29 16:47:20 +1000

    configuring aws

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "ap-southeast-2",
+      BUCKET: "int-studio-website"
+    },
+    apiGateway: {
+      REGION: "ap-southeast-2",
+      URL: "https://rxxuvqzm9d.execute-api.ap-southeast-2.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "ap-southeast-2",
+      USER_POOL_ID: "ap-southeast-2_sJCpfOPXQ",
+      APP_CLIENT_ID: "7k1g14fln33vqrhnscih927f24",
+      IDENTITY_POOL_ID: "ap-southeast-2:cfe0c817-d3c4-49dc-9745-4abc0500f964"
+    }
+  };
+  
